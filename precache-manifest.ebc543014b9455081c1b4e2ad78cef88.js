81e04534d8e24cfd285eb2bd96663ab090f76b1d
pevnev-ivan <pevnev.ivan.a@gmail.com>
2022-07-14 04:30:24 +0700

    Updates

---
+++ precache-manifest.ebc543014b9455081c1b4e2ad78cef88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b27ae3c909de0a6d97b7774e7aa05005",
+    "url": "/ignatHomeWorks/index.html"
+  },
+  {
+    "revision": "16c2594dc8a5a71bf543",
+    "url": "/ignatHomeWorks/static/css/main.ae7277c4.chunk.css"
+  },
+  {
+    "revision": "7f8be4f17f2e5682e843",
+    "url": "/ignatHomeWorks/static/js/2.6c57360a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ignatHomeWorks/static/js/2.6c57360a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16c2594dc8a5a71bf543",
+    "url": "/ignatHomeWorks/static/js/main.c3d3eb92.chunk.js"
+  },
+  {
+    "revision": "8e91112a01bc13aed37a",
+    "url": "/ignatHomeWorks/static/js/runtime-main.c3302e90.js"
+  }
+]);
