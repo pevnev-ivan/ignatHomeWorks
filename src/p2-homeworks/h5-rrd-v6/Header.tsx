aa20c57431358c03fe4baca0929f6b05257f9bc1
pevnev-ivan <pevnev.ivan.a@gmail.com>
2022-09-08 17:40:26 +0700

    HW 5  - Responsive header

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,57 +1,54 @@
-import React, {useState} from 'react'
+import React, {useRef, useState} from 'react'
 import {NavLink} from "react-router-dom";
 import h from '../../p1-main/m1-ui/u1-app/App.module.css'
+import {FaBars, FaTimes} from 'react-icons/fa'
+
 
 function Header() {
 
-    const [isActive, setIsActive] = useState(false);
-    const clickHandler = () => {
-        setIsActive(!isActive)
+    const navRef = useRef<HTMLInputElement>(null)
 
+    const showNavbar = () => {
+        navRef.current && navRef.current.classList.toggle(h.responsive_nav)
     }
 
+
     return (
-        <div>
-            <nav>
+        <header>
+            <nav ref={navRef}>
                 <span className={h.logo}>React HomeWorks</span>
 
-                <ul className={!isActive ? h.navLinks : h.navActive} >
-                    <li><NavLink
-                        style={({isActive}) => ( isActive ?{
-                            border: '2px solid whitesmoke',
-                            padding: '3px 5px',
-                            borderRadius: '20px'
-                        }: {})}
-                        className={({isActive}) => isActive ? h.actLink : h.defLink} to={'/pre-junior'}>
-                        Pre-Junior
-                    </NavLink></li>
-                    <li><NavLink
-                        style={({isActive}) => ( isActive ?{
-                            border: '2px solid whitesmoke',
-                            padding: '3px 5px',
-                            borderRadius: '20px'
-                        }: {})}
-                        className={({isActive}) => isActive ? h.actLink : h.defLink} to={'/Junior'}>
-                        Junior
-                    </NavLink></li>
-                    <li><NavLink
-                        style={({isActive}) => ( isActive ?{
-                            border: '2px solid whitesmoke',
-                            padding: '3px 5px',
-                            borderRadius: '20px'
-                        }: {})}
-                        className={({isActive}) => isActive ? h.actLink : h.defLink} to={'/Junior-plus'}>
-                        Junior+
-                    </NavLink></li>
+                <ul className={h.navLinksContainer}>
+                    <li>
+                        <NavLink
+                            className={h.singleNavLink} to={'/pre-junior'}>
+                            Pre-Junior
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            className={h.singleNavLink} to={'/Junior'}>
+                            Junior
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            className={h.singleNavLink} to={'/Junior-plus'}>
+                            Junior+
+                        </NavLink>
+                    </li>
                 </ul>
 
-                <div className={h.burger} onClick={clickHandler}>
-                    <div className={h.line1}></div>
-                    <div className={h.line2}></div>
-                    <div className={h.line3}></div>
-                </div>
+
+                <button className={`${h.nav_btn} + ${h.nav_close_btn}`}>
+                    <FaTimes onClick={showNavbar}/>
+                </button>
+
             </nav>
-        </div>
+            <button className={`${h.nav_btn}`}>
+                <FaBars onClick={showNavbar}/>
+            </button>
+        </header>
     )
 }
 
